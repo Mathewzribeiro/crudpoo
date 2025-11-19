ed5d1512b80a87f6104d1c45e6141a2062cd614c
matheus <mathewzzilva@gmail.com>
2025-11-19 19:49:02 -0300

    Penultima atualização

--- main.py
+++ main.py
@@ -26,4 +26,54 @@ print("Conta criada com sucesso!")
 
 def editar(contas):
     numero = input("Número da conta para editar: ")
-    
+    for c in contas:
+        if c.numero == numero:
+            novo_titular = input("Novo titular: ")
+            print("Conta atualizada!")
+            return
+    print("Conta não encontrada.")
+
+def remover(contas):
+    numero = input("Numero da conta para remover:")
+    for c in contas:
+        if c.numero == numero:
+            contas.remove(c)
+            print("Conta removida!")
+            return
+    print("Conta não encontrada.")
+
+def main():
+    contas = db.carregar()
+
+    while True:
+        print("\n --- Menu ---")
+        print("1 - Listar")
+        print("2 - Criar conta")
+        print("3 - Editar conta")
+        print("4 - Remover conta")
+        print("5 - Salvar e sair")
+
+        opc = input("Escolha: ")
+
+        if opc =="1":
+            listar(contas)
+        
+        elif opc == "2":
+            criar_conta(contas)
+        
+        elif opc == "3":
+            editar(contas)
+
+        elif opc == "4":
+            remover(contas)
+
+        elif opc == "2":
+            db.salvar(contas)
+            print("Dados salvos! Saindo...")
+            break
+
+        else:
+            print("Opção invalida!")
+
+        if __name__ == "__main__":
+            main()
